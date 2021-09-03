abb49161d7a4f697f4c1423f82549a6d6d999e3a
Tales Conrado <talesmateuscc@gmail.com>
2021-09-03 09:20:43 -0300

    first commit

---
+++ LiveAMessage/LiveAMessage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LiveAMessage
+//
+//  Created by Tales Conrado on 03/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
